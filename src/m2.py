9823957742d0d2bb369a2f9806df0fbd57f56da4
strainlw <strainlw@rose-hulman.edu>
2018-12-05 16:00:27 -0500

    Done

--- src/m2.py
+++ src/m2.py
@@ -15,7 +15,7 @@ import rosegraphics as rg
 
 ###############################################################################
 #
-# TODO: 2. READ these instructions, ASKING QUESTIONS as needed.
+# DONE: 2. READ these instructions, ASKING QUESTIONS as needed.
 #
 #   This module contains "broken" functions, as in m1.py.
 #   FOLLOW THE SAME STEPS as in the instructions of m1.py
